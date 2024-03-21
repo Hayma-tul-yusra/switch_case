fafda4179d8d78883755094e95eaa4b143389c4e
Hayma-tul-yusra <164422986+Hayma-tul-yusra@users.noreply.github.com>
2024-03-22 02:57:28 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const ans = await inquirer.prompt([{
+        type: "number",
+        name: "numberone",
+        message: "Enter the first number"
+    },
+    { type: "number",
+        name: "numbertwo",
+        message: "Enter the second number" },
+    { type: "list",
+        name: "oprator",
+        message: "Enter your operator",
+        choices: ["+", "-", "*", "/"] },
+]);
+const { numberone, numbertwo, oprator } = ans;
+let result;
+switch (oprator) {
+    case "+":
+        result = numberone + numbertwo;
+        break;
+    case "-":
+        result = numberone - numbertwo;
+        break;
+    case "*":
+        result = numberone * numbertwo;
+        break;
+    case "/":
+        result = numberone / numbertwo;
+        break;
+    default:
+        console.log("choose correct oprator");
+}
+console.log(chalk.yellow(`${numberone} ${oprator} ${numbertwo}= ${result}`));
